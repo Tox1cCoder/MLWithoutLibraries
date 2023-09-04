f36812262a335f7eef93ddc6dde09f340b59397c
Tox1cCoder <hroryu.nguyen@gmail.com>
2023-09-04 22:23:41 +0700

    Initial commit

---
+++ web/js/sketchPad.js
@@ -0,0 +1,46 @@
+class SketchPad {
+    constructor(container, size = 400) {
+        this.canvas = document.createElement("canvas");
+        this.canvas.width = size;
+        this.canvas.height = size;
+        this.canvas.style = `
+            background-color: white;
+            box-shadow: 0px 0px 10px 2px black;
+        `;
+        container.appendChild(this.canvas);
+
+        this.ctx = this.canvas.getContext("2d");
+
+        this.path = [];
+        this.isDrawing = false;
+
+        this.#addEventListeners();
+    }
+
+    #addEventListeners() {
+        this.canvas.onmousedown = (evt) => {
+            const rect = this.canvas.getBoundingClientRect();
+            const mouse = [
+                Math.round(evt.clientX - rect.left),
+                Math.round(evt.clientY - rect.top)
+            ];
+            this.path = [mouse];
+            this.isDrawing = true;
+        }
+
+        this.canvas.onmousemove = (evt) => {
+            if (this.isDrawing) {
+                const rect = this.canvas.getBoundingClientRect();
+                const mouse = [
+                    Math.round(evt.clientX - rect.left),
+                    Math.round(evt.clientY - rect.top)
+                ];
+                this.path.push(mouse);
+            }
+        }
+
+        this.canvas.onmouseup = (evt) => {
+            this.isDrawing = false;
+        }
+    }
+}
