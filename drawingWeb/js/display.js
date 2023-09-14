69cc6f211fc0e0b4baacd83bce7a75bb679688ff
Tox1cCoder <hroryu.nguyen@gmail.com>
2023-09-14 22:40:56 +0700

    .

--- drawingWeb/js/display.js
+++ drawingWeb/js/display.js
@@ -35,11 +35,21 @@ function createRow(container, studentName, samples) {
 }
 
 function handleClick(sample, doScroll = true) {
-    [...document.querySelectorAll('.emphasize')].
-        forEach((e) => e.classList.remove('emphasize'));
+    if (sample == null) {
+        [...document.querySelectorAll('.emphasize')].
+            forEach((e) => e.classList.remove('emphasize'));
+        return;
+    }
     const el = document.getElementById(
         "sample_" + sample.id
     );
+    if (el.classList.contains("emphasize")) {
+        el.classList.remove("emphasize");
+        chart.selectSample(null);
+        return;
+    }
+    [...document.querySelectorAll('.emphasize')].
+        forEach((e) => e.classList.remove('emphasize'));
     el.classList.add("emphasize");
     if (doScroll) {
         el.scrollIntoView({
@@ -48,4 +58,14 @@ function handleClick(sample, doScroll = true) {
         });
     }
     chart.selectSample(sample);
+}
+
+function toggleInput() {
+    if (inputContainer.style.display == "none") {
+        inputContainer.style.display = "block";
+        sketchPad.triggerUpdate();
+    } else {
+        inputContainer.style.display = "none";
+        chart.hideDynamicPoint();
+    }
 }
